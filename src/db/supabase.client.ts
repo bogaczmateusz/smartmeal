36da17a0138bd03c32a0b0679b236eed72fdfa44
Mateusz Bogacz <mateusz@martcode.pl>
2025-10-14 20:55:19 +0200

    feat: added mocked user id

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,5 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export const DEFAULT_USER_ID = "59145353-0663-42b5-883a-8b0a54081536";
